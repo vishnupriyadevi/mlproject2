d83799f566e71a2ce56dfa81fdbed65a7d3f9821
Vishnu Priya <vishnupriyadevi@gmail.com>
2025-04-24 18:29:00 -0700

    loading data

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -26,7 +26,7 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("Train test split initiated")
-            train_set,train_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             logging.info("ingestion of the data is completed")
